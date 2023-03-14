056dd7925327b694553cf1697a8f71b79c260a71
Nicholas Bowling <46771228+bowlinnr@users.noreply.github.com>
2023-03-14 15:29:21 -0400

    Uploaded Leaflet Example
    Uploaded the original Leaflet example code in a separate folder.

---
+++ Leaflet Example/js/main.js
@@ -0,0 +1,21 @@
+
+
+
+d3.tsv('data/sampleData.tsv')
+.then(data => {
+    //console.log(data[0]);
+    //console.log(data.length);
+    data.forEach(d => {
+      //console.log(d);
+      d.latitude = +d.LATITUDE; //make sure these are not strings
+      d.longitude = +d.LONGITUDE; //make sure these are not strings
+      console.log(d.latitude);
+      console.log(d.longitude);
+    });
+
+    // Initialize chart and then show it
+    leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
+
+
+  })
+  .catch(error => console.error(error));
