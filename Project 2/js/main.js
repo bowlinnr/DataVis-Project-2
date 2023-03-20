ff52952e5da833d6f589a7caa61b9e68eb01549c
Nicholas Bowling <46771228+bowlinnr@users.noreply.github.com>
2023-03-20 15:19:00 -0400

    Revert "Set Up Mock System"
    This reverts commit bb98f370f279b49b74375c5c6636978adb814a3b.

--- Project 2/js/main.js
+++ Project 2/js/main.js
@@ -1,14 +1,16 @@
 
 
 
-d3.dsv("|","data/cincy311_cleaned_reduced.tsv")
+d3.tsv('data/Cincy311_2022_final.tsv')
 .then(data => {
     //console.log(data[0]);
     //console.log(data.length);
     data.forEach(d => {
       //console.log(d);
-      d.latitude = +d.latitude; //make sure these are not strings
-      d.longitude = +d.longitude; //make sure these are not strings
+      d.latitude = +d.LATITUDE; //make sure these are not strings
+      d.longitude = +d.LONGITUDE; //make sure these are not strings
+      console.log(d.latitude);
+      console.log(d.longitude);
     });
      console.log("Data points read.")
     // Initialize chart and then show it
