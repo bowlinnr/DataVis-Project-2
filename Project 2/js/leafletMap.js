c88c689fa196e403df49e633fc89085be68606f7
William Leithauser <wleithauser@yahoo.com>
2023-03-30 17:02:48 -0400

    Revert "Add a Legend to the Map"

--- Project 2/js/leafletMap.js
+++ Project 2/js/leafletMap.js
@@ -37,44 +37,6 @@ class LeafletMap {
     vis.stUrl = 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}';
     vis.stAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-    // Dictionary to map service names to colors
-    vis.service_name_dict = {
-        "metal": "#E9F00C",
-        "trash": "#F01D0C",
-        "building": "#F47E01",
-        "default": "#8FF100",
-        "grasstree": "#02E109",
-        "pothole": "#00D591",
-        "litter": "#00D4BD",
-        "yard": "#00BFFC",
-        "recycle": "#0063FC",
-        "sign" : "#000087",
-        "sidewalk": "#9900F7",
-        "tire": "#CD8EF4",
-        "animal": "#F1A8FE",
-        "other": "#F900C1",
-    };
-
-    vis.agency_dict = {
-        "Cin Water Works": "#E9F00C",
-        "Cinc Building Dept": "#F01D0C",
-        "Cinc Health Dept": "#F47E01",
-        "Cincinnati Recreation": "#8FF100",
-        "City Manager's Office": "#02E109",
-        "Community Development": "#00D591",
-        "Dept of Trans and Eng": "#00D4BD",
-        "Enterprise Services": "#00BFFC",
-        "Fire Department": "#0063FC",
-        "Fire Dept" : "#000087",
-        "Law Department": "#9900F7",
-        "Metropolitan Sewer": "#CD8EF4",
-        "Park Department": "#F1A8FE",
-        "Police Department": "#F900C1",
-        "Public Services": "#92F1A5",
-        "Regional Computer Center": "#660E25",
-        "Treasury Department": "#9D4F4F"
-    };
-
     //this is the base map layer, where we are showing the map background
     vis.base_layer = L.tileLayer(vis.esriUrl, {
       id: 'esri-image',
@@ -123,16 +85,53 @@ class LeafletMap {
       });
 
     function color_by_service_name(sn) {
-      for (const [key, value] of Object.entries(vis.service_name_dict)) {
+      var dict = {
+        "metal": "#E9F00C",
+        "trash": "#F01D0C",
+        "building": "#F47E01",
+        "default": "#8FF100",
+        "grasstree": "#02E109",
+        "pothole": "#00D591",
+        "litter": "#00D4BD",
+        "yard": "#00BFFC",
+        "recycle": "#0063FC",
+        "sign" : "#000087",
+        "sidewalk": "#9900F7",
+        "tire": "#CD8EF4",
+        "animal": "#F1A8FE",
+        "other": "#F900C1",
+      };
+
+      for (const [key, value] of Object.entries(dict)) {
         if (sn.toUpperCase().includes(key.toUpperCase())) {
-          return vis.service_name_dict[key];
+          return dict[key];
         }
       }
-      return vis.service_name_dict["other"];
+
+      return dict["other"];
     }
 
     function color_by_agency(a) {
-      return vis.agency_dict[a]
+      var dict = {
+        "Cin Water Works": "#E9F00C",
+        "Cinc Building Dept": "#F01D0C",
+        "Cinc Health Dept": "#F47E01",
+        "Cincinnati Recreation": "#8FF100",
+        "City Manager's Office": "#02E109",
+        "Community Development": "#00D591",
+        "Dept of Trans and Eng": "#00D4BD",
+        "Enterprise Services": "#00BFFC",
+        "Fire Department": "#0063FC",
+        "Fire Dept" : "#000087",
+        "Law Department": "#9900F7",
+        "Metropolitan Sewer": "#CD8EF4",
+        "Park Department": "#F1A8FE",
+        "Police Department": "#F900C1",
+        "Public Services": "#92F1A5",
+        "Regional Computer Center": "#660E25",
+        "Treasury Department": "#9D4F4F"
+      };
+      return dict[a]
     }
 
     var requestDateColorScale = d3.scaleSequential()
@@ -174,26 +173,7 @@ class LeafletMap {
       
     }
 
-    // Create dictionary for request date legend
-    vis.called_date_dict = {}
-    let num_quantiles = 8
-    for (let i = 0; i < num_quantiles; i++) {
-      let timestamp = d3.quantile(vis.data, (i/(num_quantiles-1)), d => new Date(d.requested_datetime))
-      let date = (new Date(timestamp)).toLocaleDateString("en-US")
-      vis.called_date_dict[date] = requestDateColorScale(timestamp)
-    }
-
-    vis.response_time_dict = {}
-    num_quantiles = 10
-    for (let i = 0; i < num_quantiles; i++) {
-      let num_days = Math.floor(d3.quantile(vis.data, (i/(num_quantiles-1)), d => (new Date(d.updated_datetime)) - (new Date(d.requested_datetime))) / (1000 * 60 * 60 * 24))
-      if (num_days < 0) {
-        continue
-      }
-      vis.response_time_dict[num_days] = responseTimeColorScale2(num_days)
-    }
-
-    //these are the city locations, displayed as a set of dots
+    //these are the city locations, displayed as a set of dots 
     vis.Dots = vis.svg.selectAll('circle')
                     .data(vis.data) 
                     .join('circle')
@@ -251,27 +231,7 @@ class LeafletMap {
     .style("top", "515px")
     .append('label')
     .text('Color By: ');
-
-    // Create a legend
-    vis.legend = L.control({ position: "topright" });
-
-    // Function that recolors the legend when the coloring is changed
-    function recolor_legend(title, dict) {
-      vis.legend.remove(vis.theMap)
-      vis.legend.onAdd = function(map) {
-          var div = L.DomUtil.create("div", "legend");
-          div.innerHTML += `<h4>${title}</h4>`;
-          for (const [key, value] of Object.entries(dict)) {
-            var name = key.charAt(0).toUpperCase() + key.slice(1);
-            div.innerHTML += `<i style="background: ${value}"></i><span>${name}</span><br>`;
-          }
-          return div;
-      };
-      vis.legend.addTo(vis.theMap);
-    }
-
-    recolor_legend("Service Name", vis.service_name_dict)
-
+    
     vis.colorBySelect = d3.select('#colorBy')
       .append('select')
       .on('change', function () {
@@ -286,7 +246,6 @@ class LeafletMap {
 
               d3.select('#tooltip').style('opacity', 0);//turn off the tooltip
             })
-          recolor_legend("Service Name", vis.service_name_dict)
         }
         else if (d3.select(this).property('value') == "Agency") {
           vis.Dots
@@ -299,7 +258,6 @@ class LeafletMap {
 
               d3.select('#tooltip').style('opacity', 0);//turn off the tooltip
             })
-          recolor_legend("Agency", vis.agency_dict)
         }
         else if (d3.select(this).property('value') == "Called Date") {
           vis.Dots
@@ -312,7 +270,6 @@ class LeafletMap {
 
               d3.select('#tooltip').style('opacity', 0);//turn off the tooltip
             })
-          recolor_legend("Called Date", vis.called_date_dict)
         }
         else {
           vis.Dots
@@ -325,7 +282,6 @@ class LeafletMap {
 
               d3.select('#tooltip').style('opacity', 0);//turn off the tooltip
             })
-          recolor_legend("Response Time (days)", vis.response_time_dict)
         }
 
 
@@ -337,6 +293,7 @@ class LeafletMap {
       .enter()
       .append('option')
       .text(d => d);
+  
   }
 
   updateVis() {
