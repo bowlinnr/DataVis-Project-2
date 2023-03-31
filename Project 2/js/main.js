89456e5733aa9559b753064f6f82994c8f71aa24
William Leithauser <leithawj@mail.uc.edu>
2023-03-30 20:43:50 -0400

    Add barchart

--- Project 2/js/main.js
+++ Project 2/js/main.js
@@ -7,5 +7,34 @@ d3.dsv("|","data/cincy311_cleaned_2021.tsv")
   })
   
   timeline = new Timeline({ parentElement: '#timeline'}, data)
-  })
-  .catch(error => console.error(error));
+
+  // Translate number to day
+  let day_to_text = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+  };
+
+  // Bar chart to show which day of the week each call was made
+  weekdays = new BarChart(
+    {
+      parentElement: "#weekdays",
+      xAxisLabel: "Day of the Week",
+      yAxisLabel: "Number of Calls",
+      title: "Calls by Day",
+      xAxisLambda: (d) => {
+        return day_to_text[(new Date(d.requested_datetime)).getDay()]
+      },
+      logScale: false,
+      orderedKeys: Object.values(day_to_text),
+      containerWidth: 480,
+    },
+    data
+  );
+  weekdays.updateVis();
+
+  }).catch(error => console.error(error));
