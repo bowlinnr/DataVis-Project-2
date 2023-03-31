c2bf284e9e0f0a22d8dbf40680f8bf30c571e5b6
Kunal Kewalramani <k.kewalramani@outlook.com>
2023-03-31 12:49:21 -0400

    Experimenting with brushing on timeline

--- Project 2/js/timeline.js
+++ Project 2/js/timeline.js
@@ -5,7 +5,9 @@ class Timeline {
         parentElement: _config.parentElement,
         containerWidth: _config.containerWidth || 600,
         containerHeight: _config.containerHeight || 500,
-        margin: { top: 40, bottom: 60, right: 30, left: 60 }
+        contextHeight: 50,
+        margin: { top: 40, bottom: 150, right: 30, left: 60 },
+        contextMargin: {top: 400, right: 10, bottom: 20, left: 45}
       }
   
       this.data = _data
@@ -27,12 +29,25 @@ class Timeline {
         vis.xScale = d3.scaleTime()
             .domain(d3.extent(vis.timelineData, vis.xValue))
             .range([0, vis.width]);
+
+        vis.xScaleContext = d3.scaleTime()
+            .domain(d3.extent(vis.timelineData, vis.xValue))
+            .range([0, vis.width]);
           
         vis.yScale = d3.scaleLinear()
             .domain(d3.extent(vis.timelineData, vis.yValue))
-            .range([vis.height, 0]); 
+            .range([vis.height, 0]);
+        
+        vis.yScaleContext = d3.scaleLinear()
+            .domain(d3.extent(vis.timelineData, vis.yValue))
+            .range([vis.contextHeight, 0]); 
         
         vis.xAxis = d3.axisBottom(vis.xScale)
+            .ticks(20)
+            .tickSizeOuter(0)
+            .tickPadding(10);
+
+        vis.xAxisContext = d3.axisBottom(vis.xScaleContext)
             .ticks(20)
             .tickSizeOuter(0)
             .tickPadding(10);
@@ -101,6 +116,29 @@ class Timeline {
 
         vis.tooltip.append('text');
 
+        vis.context = vis.svg.append('g')
+            .attr('transform', `translate(${vis.config.contextMargin.left},${vis.config.contextMargin.top})`);
+
+        vis.contextAreaPath = vis.context.append('path')
+            .attr('class', 'chart-area');
+
+        vis.xAxisContextG = vis.context.append('g')
+            .attr('class', 'axis x-axis')
+            .attr('transform', `translate(0,${vis.config.contextHeight})`);
+
+        vis.brushG = vis.context.append('g')
+            .attr('class', 'brush x-brush');
+
+        // Initialize brush component
+        vis.brush = d3.brushX()
+            .extent([[0, 0], [vis.config.containerWidth, vis.config.contextHeight]])
+            .on('brush', function({selection}) {
+            if (selection) vis.brushed(selection);
+            })
+            .on('end', function({selection}) {
+            if (!selection) vis.brushed(null);
+            });
+
         vis.updateVis(); 
       }
   
@@ -122,6 +160,14 @@ class Timeline {
     vis.line = d3.line()
         .x(d => vis.xScale(vis.xValue(d)))
         .y(d => vis.yScale(vis.yValue(d)));
+
+    vis.area = d3.area()
+        .x(d => vis.xScaleContext(vis.xValue(d)))
+        .y1(d => vis.yScaleContext(vis.yValue(d)))
+        .y0(vis.config.contextHeight);
+
+    // vis.xScaleContext.domain(vis.xScale.domain());
+    // vis.yScaleContext.domain(vis.yScale.domain());
         
     vis.bisectDate = d3.bisector(vis.xValue).left;
 
@@ -138,6 +184,10 @@ class Timeline {
         .attr('class', 'line')
         .attr('stroke-width', 2)
         .attr('d', vis.line); 
+
+    // vis.contextAreaPath
+    //     .data(vis.timelineData)
+    //     .attr('d', vis.area);
 
     vis.trackingArea
         .on('mouseenter', (event, d) => {
@@ -171,6 +221,13 @@ class Timeline {
     vis.xAxisG.call(vis.xAxis);
     vis.yAxisG.call(vis.yAxis);
 
+    vis.xAxisContextG.call(vis.xAxisContext);
+
+    const defaultBrushSelection = [vis.xScale(new Date('2021-01-01')), vis.xScaleContext.range()[1]];
+    vis.brushG
+        .call(vis.brush)
+        .call(vis.brush.move, defaultBrushSelection);
+
   }
 
   arrangeData(data) {
@@ -195,6 +252,26 @@ class Timeline {
 
     console.log(dataList);
     return dataList;
+  }
+
+  brushed(selection) {
+    let vis = this;
+
+    // Check if the brush is still active or if it has been removed
+    if (selection) {
+      // Convert given pixel coordinates (range: [x0,x1]) into a time period (domain: [Date, Date])
+      const selectedDomain = selection.map(vis.xScaleContext.invert, vis.xScaleContext);
+
+      // Update x-scale of the focus view accordingly
+      vis.xScale.domain(selectedDomain);
+    } else {
+      // Reset x-scale of the focus view (full time period)
+      vis.xScale.domain(vis.xScaleContext.domain());
+    }
+
+    // Redraw line and update x-axis labels in focus view
+    vis.chart.attr('d', vis.line);
+    vis.xAxisG.call(vis.xAxis);
   }
   
 }       
