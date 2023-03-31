1f334078e2862f0324239591fa438a1e48a7fd3a
William Leithauser <leithawj@mail.uc.edu>
2023-03-31 16:48:26 -0400

    Revert outside cincinnati to other

--- Project 2/js/main.js
+++ Project 2/js/main.js
@@ -71,13 +71,12 @@ d3.dsv("|","data/cincy311_cleaned_2021.tsv")
     yAxisLabel: "Number of Calls",
     title: "Calls by Zipcode",
     xAxisLambda: (d) => {
-      return top_zipcodes.has(+d['zipcode']) ? Math.trunc(+d['zipcode']) : "Outside Cincinnati"
+      return top_zipcodes.has(+d['zipcode']) ? Math.trunc(+d['zipcode']) : "Other"
     },
     logScale: false,
     tiltTicks: true,
     containerWidth: 660,
-    margin: { top: 60, bottom: 80, right: 20, left: 70 },
-    no_data_key: "Outside Cincinnati",
+    no_data_key: "Other",
   },
   data
   );
